26e8209114f5a62e574af5e43f5b69d19b948ef9
EQUIPO <EQUIPO@DESKTOP-7O9LHSC>
2022-04-23 04:10:49 -0500

    Add project files.

---
+++ ParcialZ/Helpers/ICombosHelper.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using ParcialZ.Data.Entities;
+
+namespace ParcialZ.Helpers
+{
+    public interface ICombosHelper
+    {
+        Task<IEnumerable<SelectListItem>> GetComboEntrancesAsync();
+        Task<IEnumerable<SelectListItem>> GetComboEntrancesAsync(IEnumerable<Entrance> filter);
+    }
+}
